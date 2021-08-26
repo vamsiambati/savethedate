223ed9f3da19446e4353ed5bff7ad70d80e561ad
Vamsi Ambati <ambati.vamsi96@gmail.com>
2021-08-27 00:03:08 +0530

    timer and venue point

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/**
+* Template Name: ComingSoon - v2.2.0
+* Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
+  "use strict";
+  var cndDate = new Date('2021/09/01')
+  cndDate.setHours(9)
+  cndDate.setMinutes(30)
+  if ($('.countdown').length) {
+    var template = $('.countdown').html();
+    $('.countdown').countdown(cndDate, function(event) {
+      $(this).html(event.strftime(template));
+    });
+  }
+  
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+  // $(document).(function(){
+  //   $('.mainTitle').text(mainTitle);
+  // })
+
+})(jQuery);
